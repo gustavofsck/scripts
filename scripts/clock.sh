442f9c99e1a51e49c27465a62f59f7a2e1943ef6
Gustavo Martins <gustavomartinsrms@proton.me>
2026-04-03 13:43:38 -0300

    clock.sh

---
+++ scripts/clock.sh
@@ -0,0 +1,6 @@
+while true; do date +'%A %d/%m/%y %H:%M:%S' | xsetroot -name "$(cat)"; sleep 1; done &
+
+#while true; do printf "                                                                                                                                                                                                                                                                                                                                       " | xsetroot -name "$(cat)"; sleep 1; done &
+
+
+#while true; do date | xsetroot -name "$(cat)"; sleep 1; done &
