4ff1ec22a21c4cd8f0f8e0cd45df41b899bc275b
Gustavo Martins <gustavomartinsrms@proton.me>
2026-04-15 21:53:07 -0300

    Remove misplaced files from repo

--- fix-monitors.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/dash
-
-xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240 --primary && xrandr --output HDMI-1-2 --mode 1920x1080 --rate 60 --left-of DisplayPort-2
--- start-polkit-agent.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/dash
-
-# gnome polkit
-#/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-
-#lxsession polkit
-lxpolkit &
